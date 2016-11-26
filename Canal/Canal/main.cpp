d504032db86a9f0675d760de10151141af9f34c2
nmerloleiva <nmerloleiva@gmail.com>
2016-11-26 00:14:21 -0300

    Project setup.

---
+++ Canal/Canal/main.cpp
@@ -0,0 +1,21 @@
+/*
+**	Universidad de Buenos Aires
+**	Facultad de Ingeniería
+**	75.12 Análisis Numérico I
+**	Trabajo Práctico 2
+**	Curso 3
+**	26/11/2016
+**
+**	Merlo Leiva Nahuel
+**	Padrón 92115
+*/
+
+#include <string>
+
+int main()
+{
+	printf("Merlo Leiva Nahuel\n");
+	printf("Padrón 92115\n");
+	getchar();
+	return 0;
+}
