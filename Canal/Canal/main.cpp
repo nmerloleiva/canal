d6fbba5df6bddec6daf44a2d63a4a254960609c7
nmerloleiva <nmerloleiva@gmail.com>
2016-11-27 11:03:13 -0300

    Simulation context.

--- Canal/Canal/main.cpp
+++ Canal/Canal/main.cpp
@@ -19,15 +19,14 @@
 #include <fstream>
 #include <string>
 #include <iomanip>
-
+#include <iostream>
+#include <locale> 
 
 // Precision type abstraction.
 typedef double REAL;
 #define REAL_MAX std::numeric_limits<REAL>::max()
 
 #define PANAMA_COCOLI_LOCKS_LATITUDE 8.985470
-#define MAX_OSCILATION_AMPLITUDE 0.1
-#define MAX_TRUNCATION_ERROR 0.01
 
 struct REAL_PAIR
 {
@@ -56,41 +55,95 @@ const auto Kv_table_size = sizeof(Kv_table) / sizeof(Kv_table[0]);
 
 struct SOLVE_CONTEXT
 {
-	REAL tap;
-	REAL t;
-	REAL k;
-	REAL H_1_n;
-	REAL H_2_n;
-	REAL Q_n;
-	REAL H_1_n_1;
-	REAL H_2_n_1;
-	REAL Q_n_1;
+	REAL tap = 0;
+	REAL t = 0;
+	REAL k = 0;
+	REAL H_1_n = 0;
+	REAL H_2_n = 0;
+	REAL Q_n = 0;
+	REAL H_1_n_1 = 0;
+	REAL H_2_n_1 = 0;
+	REAL Q_n_1 = 0;
 
-	REAL const_1;
-	REAL const_2;
-	REAL const_3;
+	REAL const_1 = 0;
+	REAL const_2 = 0;
+	REAL const_3 = 0;
 
-	REAL var_1;
-	REAL var_2;
+	REAL var_1 = 0;
+	REAL var_2 = 0;
 
-	REAL H_2_max;
-	REAL H_2_delta;
-	REAL H_2_osc_min;
-	REAL H_2_osc_max;
-	REAL H_1_truncation_error;
-	REAL H_2_truncation_error;
+	REAL H_2_max = 0;
+	REAL H_2_delta = 0;
+	REAL H_2_osc_min = 0;
+	REAL H_2_osc_max = 0;
+	REAL H_1_truncation_error = 0;
+	REAL H_2_truncation_error = 0;
 };
 
 struct CANAL_CONTEXT
 {
-	REAL omega_1;
-	REAL omega_2;
-	REAL g;
-	REAL A;
-	REAL L;
-	REAL D_e;
-	REAL f;
-	REAL K_e;
+	REAL omega_1 = 0;
+	REAL omega_2 = 0;
+	REAL g = 0;
+	REAL A = 0;
+	REAL L = 0;
+	REAL D_e = 0;
+	REAL f = 0;
+	REAL K_e = 0;
+};
+
+struct SIMULATION_STEP
+{
+	REAL time = 0;
+	REAL H_1 = 0;
+	REAL H_2 = 0;
+};
+
+struct SIMULATION_RESULT
+{
+	std::vector<SIMULATION_STEP> steps;
+	REAL k = 0;
+	REAL H_2_max = 0;
+	
+	REAL GetTotalTime()
+	{
+		return steps.back().time;
+	}
+
+	void AddStep(REAL time, REAL H_1, REAL H_2)
+	{
+		SIMULATION_STEP step;
+		step.time = time;
+		step.H_1 = H_1;
+		step.H_2 = H_2;
+		steps.push_back(step);
+
+		if (H_2 > H_2_max)
+		{
+			H_2_max = H_2;
+		}
+	}
+
+	void Reset(REAL k)
+	{
+		this->steps.clear();
+		this->k = k;
+		this->H_2_max = 0;
+	}
+};
+
+struct SIMULATION_CONTEXT
+{
+	SIMULATION_RESULT result;
+	CANAL_CONTEXT canalContext;
+	REAL openTime = 0;
+	REAL oscillationTime = 0;
+	REAL closeTime = 0;
+	REAL maxOscillationAmplitude = 0;
+	REAL startStep = 0;
+	REAL maxTruncationError = 0;
+	REAL startHeight1 = 0;
+	REAL startHeight2 = 0;
 };
 
 REAL ComputeKv(REAL openPercent)
@@ -140,47 +193,46 @@ REAL ComputeGravityFromLatitude(REAL l)
 	return 9.780327 * (1.0 + 0.0053024 * pow(sin(l), 2) - 0.0000058 * pow(sin(2 * l), 2));
 }
 
-int main()
+//std::fstream file;
+//std::string fileName = "openLockSimulation.csv";
+//file.open(fileName, std::ios_base::out);
+//file << "Simulation start k=" << solveContext.k << "\n";
+
+//if (floor(solveContext.t) == solveContext.t)
+//{
+//	file << solveContext.t << ";" << solveContext.H_1_n << ";" << solveContext.H_2_n << ";" << "\n";
+//}
+
+//file << "Simulation abort:" << " truncation error exceeded limit!" << " H1 error=" << truncationErrorH_1 << " H2 error=" << truncationErrorH_2 << "\n" << "\n";
+//file << "Simulation completed!" << "\n";
+//file.close();
+
+void RunCanalSimulation(SIMULATION_CONTEXT& simulation)
 {
-	printf("Merlo Leiva Nahuel\n");
-	printf("Padrón 92115\n");
+	CANAL_CONTEXT canalContext = simulation.canalContext;
 
-	CANAL_CONTEXT canalContext = { 0 };
-	canalContext.omega_1 = 28000; // m^2
-	canalContext.omega_2 = 28000; // m^2
-	canalContext.g = ComputeGravityFromLatitude(PANAMA_COCOLI_LOCKS_LATITUDE); // m/s^2 
-	canalContext.A = 8.30 * 6.50; // m
-	canalContext.L = 600; // m
-	canalContext.D_e = sqrt((4 * canalContext.A) / M_PI); // m
-	canalContext.f = 0.02;
-	canalContext.K_e = 0;
-
-	SOLVE_CONTEXT solveContext = { 0 };
+	SOLVE_CONTEXT solveContext;
 	solveContext.const_1 = (canalContext.g * canalContext.A) / canalContext.L;
 	solveContext.const_2 = canalContext.f / (2.0 * canalContext.D_e * canalContext.A);
 	solveContext.const_3 = canalContext.K_e / (2.0 * canalContext.A * canalContext.L);
-	solveContext.tap = 240;
-	solveContext.k = 1;
-
-
-	std::fstream file;
-	std::string fileName = "simulationRun.txt";
-	file.open(fileName, std::ios_base::out);
+	solveContext.tap = simulation.openTime; // s
+	solveContext.k = simulation.startStep; // s
+	
+	simulation.result.Reset(solveContext.k);
 
 	bool truncationErrorBelowMax = false;
 	while (!truncationErrorBelowMax)
 	{
-		file << "Simulation start k=" << solveContext.k << "\n";
-		solveContext.t = 0;
-		solveContext.H_1_n = 18.0; // H_1_0
-		solveContext.H_2_n = 0.0; // H_2_0
+		solveContext.t = 0; // s
+		solveContext.H_1_n = simulation.startHeight1; // H_1_0
+		solveContext.H_2_n = simulation.startHeight2; // H_2_0
 		solveContext.Q_n = 0.0; // Q_0
-		solveContext.H_2_delta = solveContext.H_2_n_1 - solveContext.H_2_n;
+		solveContext.H_2_delta = solveContext.H_2_n_1 - solveContext.H_2_n; // m
 
 		truncationErrorBelowMax = true;
 		bool openTimeHit = solveContext.t >= solveContext.tap;
 		bool oscillationHit = solveContext.H_1_n < solveContext.H_2_n;
-		bool stableOscillationHit = abs(solveContext.H_1_n - solveContext.H_2_n) < MAX_OSCILATION_AMPLITUDE;
+		bool stableOscillationHit = abs(solveContext.H_1_n - solveContext.H_2_n) < simulation.maxOscillationAmplitude;
 		while (truncationErrorBelowMax &&
 			(!openTimeHit ||
 			!oscillationHit ||
@@ -195,39 +247,34 @@ int main()
 			}
 
 			CanalSolveRK2Step(canalContext, solveContext);
-			
-			file << "Simulation step" << " t=" << std::setw(14) << solveContext.t << " H1=" << std::setw(14) << solveContext.H_1_n << " H2=" << std::setw(14) << solveContext.H_2_n << "\n";
+
+			simulation.result.AddStep(solveContext.t, solveContext.H_1_n, solveContext.H_2_n);
 
 			REAL truncationErrorH_1 = abs(solveContext.H_1_n_1 - solveContext.H_1_n);
 			REAL truncationErrorH_2 = abs(solveContext.H_2_n_1 - solveContext.H_2_n);
-			if (truncationErrorH_1 > MAX_TRUNCATION_ERROR ||
-				truncationErrorH_2 > MAX_TRUNCATION_ERROR)
+			if (truncationErrorH_1 > simulation.maxTruncationError ||
+				truncationErrorH_2 > simulation.maxTruncationError)
 			{
 				truncationErrorBelowMax = false;
 				solveContext.k /= 2;
-				file << "Simulation abort:" << " truncation error exceeded limit!" << " H1 error=" << truncationErrorH_1 << " H2 error=" << truncationErrorH_2 << "\n" << "\n";
+				simulation.result.Reset(solveContext.k);
 			}
 			else
 			{
 				truncationErrorBelowMax = true;
-			
+
 				if (!openTimeHit)
 				{
 					openTimeHit = solveContext.t >= solveContext.tap;
 				}
 				else
 				{
-					if (solveContext.H_2_n_1 > solveContext.H_2_max)
-					{
-						solveContext.H_2_max = solveContext.H_2_n_1;
-					}
-
 					if (solveContext.H_2_delta < 0 &&
 						solveContext.H_2_n_1 > solveContext.H_2_n)
 					{
 						solveContext.H_2_osc_min = solveContext.H_2_n;
 					}
-					else if (solveContext.H_2_delta > 0 && 
+					else if (solveContext.H_2_delta > 0 &&
 						solveContext.H_2_n_1 < solveContext.H_2_n)
 					{
 						solveContext.H_2_osc_max = solveContext.H_2_n;
@@ -237,9 +284,9 @@ int main()
 					{
 						oscillationHit = solveContext.H_2_osc_min > 0 && solveContext.H_2_osc_max > 0;
 					}
-					else 
+					else
 					{
-						stableOscillationHit = abs(solveContext.H_2_osc_max - solveContext.H_2_osc_min) < MAX_OSCILATION_AMPLITUDE;
+						stableOscillationHit = abs(solveContext.H_2_osc_max - solveContext.H_2_osc_min) < simulation.maxOscillationAmplitude;
 					}
 				}
 
@@ -251,8 +298,32 @@ int main()
 			}
 		}
 	}
+}
 
-	file << "Simulation completed!" << "\n";
-	file.close();
+int main()
+{
+	printf("Merlo Leiva Nahuel\n");
+	printf("Padrón 92115\n");
+
+	CANAL_CONTEXT canalContext;
+	canalContext.omega_1 = 28000; // m^2
+	canalContext.omega_2 = 28000; // m^2
+	canalContext.g = ComputeGravityFromLatitude(PANAMA_COCOLI_LOCKS_LATITUDE); // m/s^2 
+	canalContext.A = 8.30 * 6.50; // m
+	canalContext.L = 600; // m
+	canalContext.D_e = sqrt((4 * canalContext.A) / M_PI); // m
+	canalContext.f = 0.02;
+	canalContext.K_e = 0;
+
+	SIMULATION_CONTEXT simulation1;
+	simulation1.canalContext = canalContext;
+	simulation1.maxTruncationError = 0.01;
+	simulation1.maxOscillationAmplitude = 0.1;
+	simulation1.openTime = 240;
+	simulation1.startHeight1 = 18;
+	simulation1.startHeight2 = 0;
+	simulation1.startStep = 1;
+	RunCanalSimulation(simulation1);
+
 	return 0;
 }
