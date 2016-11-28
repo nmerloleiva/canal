270277b2dfa6c1c7e2f1b500fa3ee1bbde012aad
nmerloleiva <nmerloleiva@gmail.com>
2016-11-27 22:13:22 -0300

    RK4 impl.

--- Canal/Canal/main.cpp
+++ Canal/Canal/main.cpp
@@ -69,6 +69,7 @@ struct SOLVE_CONTEXT
 	REAL const_3 = 0;
 
 	REAL var_1 = 0;
+	REAL var_1_2 = 0;
 	REAL var_2 = 0;
 
 	REAL H_2_max = 0;
@@ -111,6 +112,7 @@ struct SIMULATION_RESULT
 	std::vector<SIMULATION_STEP> steps;
 	REAL k = 0;
 	REAL H_2_max = 0;
+	REAL Q_max = 0;
 	
 	REAL GetTotalTime()
 	{
@@ -130,6 +132,10 @@ struct SIMULATION_RESULT
 		{
 			H_2_max = H_2;
 		}
+		if (abs(Q) > Q_max)
+		{
+			Q_max = abs(Q);
+		}
 	}
 
 	void Reset(REAL k)
@@ -137,8 +143,11 @@ struct SIMULATION_RESULT
 		this->steps.clear();
 		this->k = k;
 		this->H_2_max = 0;
+		this->Q_max = 0;
 	}
 };
+
+typedef void(SOLVE_METHOD)(CANAL_CONTEXT&, SOLVE_CONTEXT&);
 
 struct SIMULATION_CONTEXT
 {
@@ -153,6 +162,7 @@ struct SIMULATION_CONTEXT
 	REAL maxTruncationError = 0;
 	REAL startHeight1 = 0;
 	REAL startHeight2 = 0;
+	SOLVE_METHOD* solveMethod = nullptr;
 };
 
 REAL ComputeKv(REAL openPercent)
@@ -194,6 +204,7 @@ void CanalSolveRK2Step(CANAL_CONTEXT& c, SOLVE_CONTEXT& s)
 		s.const_2 * s.Q_n * abs(s.Q_n) -
 		s.const_3 * s.Q_n * abs(s.Q_n) -
 		s.var_1 * s.Q_n * abs(s.Q_n));
+
 	REAL q_2_H_1 = s.k * ((-2 * (s.Q_n + q_1_Q)) / c.omega_1);
 	REAL q_2_H_2 = s.k * ((2 * (s.Q_n + q_1_Q)) / c.omega_2);
 	REAL q_2_Q = s.k * (
@@ -201,29 +212,55 @@ void CanalSolveRK2Step(CANAL_CONTEXT& c, SOLVE_CONTEXT& s)
 		s.const_2 * (s.Q_n + q_1_Q) * abs((s.Q_n + q_1_Q)) -
 		s.const_3 * (s.Q_n + q_1_Q)* abs((s.Q_n + q_1_Q)) -
 		s.var_2 * (s.Q_n + q_1_Q) * abs((s.Q_n + q_1_Q)));
+
 	s.H_1_n_1 = s.H_1_n + 0.5 * (q_1_H_1 + q_2_H_1);
 	s.H_2_n_1 = s.H_2_n + 0.5 * (q_1_H_2 + q_2_H_2);
 	s.Q_n_1 = s.Q_n + 0.5 * (q_1_Q + q_2_Q);
+}
+
+void CanalSolveRK4Step(CANAL_CONTEXT& c, SOLVE_CONTEXT& s)
+{
+	REAL q_1_H_1 = s.k * ((-2 * s.Q_n) / c.omega_1);
+	REAL q_1_H_2 = s.k * ((2 * s.Q_n) / c.omega_2);
+	REAL q_1_Q = s.k * (
+		s.const_1 * (s.H_1_n - s.H_2_n) -
+		s.const_2 * s.Q_n * abs(s.Q_n) -
+		s.const_3 * s.Q_n * abs(s.Q_n) -
+		s.var_1 * s.Q_n * abs(s.Q_n));
+
+	REAL q_2_H_1 = s.k * ((-2 * (s.Q_n + 0.5 * q_1_Q)) / c.omega_1);
+	REAL q_2_H_2 = s.k * ((2 * (s.Q_n + 0.5 * q_1_Q)) / c.omega_2);
+	REAL q_2_Q = s.k * (
+		s.const_1 * (s.H_1_n + 0.5 * q_1_H_1 - (s.H_2_n + 0.5 * q_1_H_2)) -
+		s.const_2 * (s.Q_n + 0.5 * q_1_Q) * abs((s.Q_n + 0.5 * q_1_Q)) -
+		s.const_3 * (s.Q_n + 0.5 * q_1_Q)* abs((s.Q_n + 0.5 * q_1_Q)) -
+		s.var_1_2 * (s.Q_n + 0.5 * q_1_Q) * abs((s.Q_n + 0.5 * q_1_Q)));
+
+	REAL q_3_H_1 = s.k * ((-2 * (s.Q_n + 0.5 * q_2_Q)) / c.omega_1);
+	REAL q_3_H_2 = s.k * ((2 * (s.Q_n + 0.5 * q_2_Q)) / c.omega_2);
+	REAL q_3_Q = s.k * (
+		s.const_1 * (s.H_1_n + 0.5 * q_2_H_1 - (s.H_2_n + 0.5 * q_2_H_2)) -
+		s.const_2 * (s.Q_n + 0.5 * q_2_Q) * abs((s.Q_n + 0.5 * q_2_Q)) -
+		s.const_3 * (s.Q_n + 0.5 * q_2_Q)* abs((s.Q_n + 0.5 * q_2_Q)) -
+		s.var_1_2 * (s.Q_n + 0.5 * q_2_Q) * abs((s.Q_n + 0.5 * q_2_Q)));
+
+	REAL q_4_H_1 = s.k * ((-2 * (s.Q_n + q_3_Q)) / c.omega_1);
+	REAL q_4_H_2 = s.k * ((2 * (s.Q_n + q_3_Q)) / c.omega_2);
+	REAL q_4_Q = s.k * (
+		s.const_1 * (s.H_1_n + q_3_H_1 - (s.H_2_n + q_3_H_2)) -
+		s.const_2 * (s.Q_n + q_3_Q) * abs((s.Q_n + q_3_Q)) -
+		s.const_3 * (s.Q_n + q_3_Q)* abs((s.Q_n + q_3_Q)) -
+		s.var_2 * (s.Q_n + q_3_Q) * abs((s.Q_n + q_3_Q)));
+
+	s.H_1_n_1 = s.H_1_n + (q_1_H_1 + 2 * q_2_H_1 + 2 * q_3_H_1 + q_4_H_1) / 6.0;
+	s.H_2_n_1 = s.H_2_n + (q_1_H_2 + 2 * q_2_H_2 + 2 * q_3_H_2 + q_4_H_2) / 6.0;
+	s.Q_n_1 = s.Q_n + (q_1_Q + 2 * q_2_Q + 2 * q_3_Q + q_4_Q) / 6.0;
 }
 
 REAL ComputeGravityFromLatitude(REAL l)
 {
 	return 9.780327 * (1.0 + 0.0053024 * pow(sin(l), 2) - 0.0000058 * pow(sin(2 * l), 2));
 }
-
-//std::fstream file;
-//std::string fileName = "openLockSimulation.csv";
-//file.open(fileName, std::ios_base::out);
-//file << "Simulation start k=" << solveContext.k << "\n";
-
-//if (floor(solveContext.t) == solveContext.t)
-//{
-//	file << solveContext.t << ";" << solveContext.H_1_n << ";" << solveContext.H_2_n << ";" << "\n";
-//}
-
-//file << "Simulation abort:" << " truncation error exceeded limit!" << " H1 error=" << truncationErrorH_1 << " H2 error=" << truncationErrorH_2 << "\n" << "\n";
-//file << "Simulation completed!" << "\n";
-//file.close();
 
 void RunCanalSimulation(SIMULATION_CONTEXT& simulation)
 {
@@ -243,6 +280,8 @@ void RunCanalSimulation(SIMULATION_CONTEXT& simulation)
 		solveContext.t = 0; // s
 		solveContext.H_1_n = simulation.startHeight1; // H_1_0
 		solveContext.H_2_n = simulation.startHeight2; // H_2_0
+		solveContext.H_1_n_1 = 0; 
+		solveContext.H_2_n_1 = 0;
 		solveContext.Q_n = 0.0; // Q_0
 		solveContext.H_2_delta = solveContext.H_2_n_1 - solveContext.H_2_n; // m
 
@@ -260,8 +299,10 @@ void RunCanalSimulation(SIMULATION_CONTEXT& simulation)
 			if (solveContext.t <= simulation.openTime)
 			{
 				REAL Kv_n = ComputeKv(solveContext.t / simulation.openTime);
+				REAL Kv_n_1_2 = ComputeKv((solveContext.t + (solveContext.k / 2)) / simulation.openTime);
 				REAL Kv_n_1 = ComputeKv((solveContext.t + solveContext.k) / simulation.openTime);
 				solveContext.var_1 = Kv_n / (2.0 * canalContext.A * canalContext.L);
+				solveContext.var_1_2 = Kv_n_1_2 / (2.0 * canalContext.A * canalContext.L);
 				solveContext.var_2 = Kv_n_1 / (2.0 * canalContext.A * canalContext.L);
 			}
 			else if (solveContext.t >= simulation.closeStartTime &&
@@ -269,12 +310,14 @@ void RunCanalSimulation(SIMULATION_CONTEXT& simulation)
 			{
 				REAL closeTimeLeft = simulation.closeTime - (solveContext.t - simulation.closeStartTime);
 				REAL Kv_n = ComputeKv(closeTimeLeft / simulation.closeTime);
+				REAL Kv_n_1_2 = ComputeKv((closeTimeLeft - (solveContext.k / 2)) / simulation.closeTime);
 				REAL Kv_n_1 = ComputeKv((closeTimeLeft - solveContext.k) / simulation.closeTime);
 				solveContext.var_1 = Kv_n / (2.0 * canalContext.A * canalContext.L);
+				solveContext.var_1_2 = Kv_n_1_2 / (2.0 * canalContext.A * canalContext.L);
 				solveContext.var_2 = Kv_n_1 / (2.0 * canalContext.A * canalContext.L);
 			}
 
-			CanalSolveRK2Step(canalContext, solveContext);
+			simulation.solveMethod(canalContext, solveContext);
 
 			simulation.result.AddStep(solveContext.t, solveContext.H_1_n, solveContext.H_2_n, solveContext.Q_n);
 
@@ -286,7 +329,7 @@ void RunCanalSimulation(SIMULATION_CONTEXT& simulation)
 					truncationErrorH_2 > simulation.maxTruncationError)
 				{
 					truncationErrorBelowMax = false;
-					solveContext.k /= 2;
+					solveContext.k /= 10;
 					simulation.result.Reset(solveContext.k);
 				}
 				else
@@ -421,12 +464,12 @@ void RunKeAdjustment(SIMULATION_CONTEXT& simulation, std::string testFileName)
 void SaveResult(SIMULATION_CONTEXT& context, std::string fileName)
 {
 	std::fstream file;
-	std::vector<TEST_STEP> testSteps;
 	file.open(fileName, std::ios_base::out);
 	file << "k;" << context.result.k << "\n";
 	file << "f;" << context.canalContext.f << "\n";
 	file << "ke;" << context.canalContext.K_e << "\n";
 	file << "max H2;" << context.result.H_2_max << "\n";
+	file << "max Q;" << context.result.Q_max << "\n";
 	REAL oscillationt = 0;
 	if (context.closeStartTime)
 	{
@@ -472,6 +515,7 @@ int main()
 	simulation0.startHeight1 = 18;
 	simulation0.startHeight2 = 0;
 	simulation0.startStep = 1;
+	simulation0.solveMethod = CanalSolveRK2Step;
 	RunCanalSimulation(simulation0);
 	SaveResult(simulation0, "simulation0.result.csv");
 
@@ -479,6 +523,7 @@ int main()
 	SIMULATION_CONTEXT simulation1;
 	simulation1.canalContext = canalContext;
 	simulation1.startStep = simulation0.result.k;
+	simulation1.solveMethod = CanalSolveRK2Step;
 	RunKeAdjustment(simulation1, "simulation1.csv");
 	SaveResult(simulation1, "simulation1.result.csv");
 
@@ -486,6 +531,7 @@ int main()
 	SIMULATION_CONTEXT simulation2;
 	simulation2.canalContext = canalContext;
 	simulation2.startStep = simulation0.result.k;
+	simulation2.solveMethod = CanalSolveRK2Step;
 	RunKeAdjustment(simulation2, "simulation2.csv");
 	SaveResult(simulation2, "simulation2.result.csv");
 
@@ -493,6 +539,7 @@ int main()
 	SIMULATION_CONTEXT simulation3;
 	simulation3.canalContext = canalContext;
 	simulation3.startStep = simulation0.result.k;
+	simulation3.solveMethod = CanalSolveRK2Step;
 	RunKeAdjustment(simulation3, "simulation3.csv");
 	SaveResult(simulation3, "simulation3.result.csv");
 
@@ -500,8 +547,22 @@ int main()
 	SIMULATION_CONTEXT simulation4;
 	simulation4.canalContext = canalContext;
 	simulation4.startStep = simulation0.result.k;
+	simulation4.solveMethod = CanalSolveRK2Step;
 	RunKeAdjustment(simulation4, "simulation4.csv");
 	SaveResult(simulation4, "simulation4.result.csv");
+
+	printf("Run simulation 5...\n");
+	SIMULATION_CONTEXT simulation5;
+	simulation5.canalContext = canalContext;
+	simulation5.maxTruncationError = 0.01;
+	simulation5.maxOscillationAmplitude = 0.1;
+	simulation5.openTime = 240;
+	simulation5.startHeight1 = 18;
+	simulation5.startHeight2 = 0;
+	simulation5.startStep = 1;
+	simulation5.solveMethod = CanalSolveRK4Step;
+	RunCanalSimulation(simulation5);
+	SaveResult(simulation5, "simulation5.result.csv");
 
 	return 0;
 }
